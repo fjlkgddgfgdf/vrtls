9758b199c33efcd92a9d57ddc2684db154a34746
konantovich <hmasteraaa@gmail.com>
2025-05-29 20:28:42 +0300

    fixed tg notify

--- src/app/chat/[character]/generateStaticParams.ts
+++
@@ -1,7 +0,0 @@
-import { characterDetails } from '../../data/characters';
-
-export async function generateStaticParams() {
-  return characterDetails.map((character) => ({
-    character: character.id,
-  }));
-} 
